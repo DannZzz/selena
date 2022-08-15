a7a0cb3725a631c1ae00b71e2e5a0c11a38c2cb9
Dann <46873266+Aeol-ian@users.noreply.github.com>
2022-08-15 19:46:28 +0400

    Add files via upload

--- src/database/models/Settings.ts
+++ src/database/models/Settings.ts
@@ -4,12 +4,14 @@ import { ObjectType } from "../../structures/MainTypes";
 export interface Settings {
     _id: string
     lastDailyTime: Date
+    apiTokens: string[]
 }
 
 export const Settings = mongoose.model("settings", new mongoose.Schema<Settings>({
     _id: String,
-    lastDailyTime: { type: Date, default: Date.now }
+    lastDailyTime: { type: Date, default: Date.now },
+    apiTokens: { type: Array as any, default: [] }
 }))
 
-export const SettingsKeys: readonly (keyof Settings)[] = ["_id", "lastDailyTime"];
+export const SettingsKeys: readonly (keyof Settings)[] = ["_id", "lastDailyTime", "apiTokens"];
 export const SettingsKeysApi: readonly (keyof Settings)[] = ["_id", "lastDailyTime"];
