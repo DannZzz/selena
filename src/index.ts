56a4efb5e38b90556a57e4f83b55c5816c26ef71
Dann <46873266+Aeol-ian@users.noreply.github.com>
2022-08-16 22:04:02 +0400

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 import { Client } from "client-discord";
-import { GatewayIntentBits } from "discord.js";
+import { GatewayIntentBits, Partials } from "discord.js";
 import { DASocket } from "donationalerts-api";
 import { ColorObject, DonationAlertsToken, TOKEN } from "./config";
 import HandleDonationAlerts from "./custom-modules/DonationAlerts";
 import { HandleAll } from "./handlers/handler";
-export const client = new Client({allowedMentions: {parse: ["users"]}, token: TOKEN, intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages], colors: ColorObject as any});
+export const client = new Client({allowedMentions: {parse: ["users"]}, token: TOKEN, intents: [
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildMessageReactions,
+], colors: ColorObject as any,
+partials: [
+    Partials.Reaction,
+    Partials.User,
+    Partials.GuildMember,
+    Partials.Message,
+    Partials.Channel,
+]});
 HandleAll(client);
 const DaSocket = new DASocket(DonationAlertsToken);
 HandleDonationAlerts(DaSocket);
